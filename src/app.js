fdc2f1a49de45a56800f8eb9017b8ed951008da4
aryalbishnu <ariyalbishnu@gmail.com>
2023-11-12 19:03:46 +0900

    change something

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require("express");
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const hbs = require('hbs');
 const jwt = require('jsonwebtoken');
 const User = require('./models/user');
@@ -19,6 +20,7 @@ hbs.registerPartials(partial_path);
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
 app.use(userRouter);
 /*
 const jwtConnect = async()=>{
--- src/routers/router.js
+++ src/routers/router.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const auth = require('../auths/auth');
 const bcrypt = require('bcrypt');
 
 const router = new express.Router();
+router.use(cookieParser());
 
 router.get('/', (req, res) =>{
     res.render('register');
@@ -24,21 +27,34 @@ router.post('/register', async(req, res) =>{
                 repassword: req.body.repassword
             });
             // token generate function call
-            await user.generateToken();
+            const token = await user.generateToken();
+            // cookie set
+            res.cookie("jwt", token, {
+                expires: new Date(Date.now() + 300000),
+                httpOnly: true
+            });
+            
             await user.save();
             res.status(201).render("login");
         }else {
-            res.status(401).send("password is wrong");
+            res.status(401).render("register", {
+                errorMessage: "please enter a same password",
+                errorMsg: "Something is worng..."
+            });
         }
         
     } catch (error) {
-        res.status(400).send(error);
+        res.status(500).send(error);
     }
     
 })
 
 router.get('/login', (req, res) =>{
     res.render('login');
+});
+
+router.get('/home', (req, res) =>{
+    res.render('home');
 });
 
 router.post('/login', async(req, res) =>{
@@ -50,19 +66,50 @@ router.post('/login', async(req, res) =>{
             const passwordMatch = await bcrypt.compare(password, user.password);
             if (passwordMatch){
                 // generate token
-                await user.generateToken();
-                res.status(200).send("congratulation");
+                const token = await user.generateToken();
+                // cookie set
+                res.cookie("jwt", token, {
+                expires: new Date(Date.now() + 300000),
+                httpOnly: true
+            });
+                res.status(200).render("home");
             }else {
-                res.status(404).send("password is wrong");
+                res.status(404).render("login", {
+                    errorMessage: "please enter a same password",
+                    errorMsg: "Something is worng..." 
+                });
             }
         } else {
-            res.status(404).send("user is not exits");
+            res.status(404).render("login", {
+                errorMsg: "user is not exits"
+            });
         }
         
     } catch (error) {
         res.status(500).send(error);
     }
     
+});
+
+router.get('/private' , auth, (req, res) =>{
+    res.render('private');
+});
+
+router.get('/logout' , auth, async(req, res) =>{
+    try {
+        // particular token remove
+        /*
+        req.user.tokens = req.user.tokens.filter((currentToken) =>{
+            return currentToken.token !== req.token;
+        });*/
+        // all token delete 
+        req.user.tokens = [];
+        res.clearCookie("jwt");
+        await req.user.save();
+        res.render('login');
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 module.exports = router;
